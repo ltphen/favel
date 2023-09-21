42b2d5c05b23e1b05856c5cc5de50d90616a8263
ltphen <tatkeulionel@gmail.com>
2023-09-21 06:33:25 +0200

    deleted time left for tasks

--- Software/MLService/ML.py
+++ Software/MLService/ML.py
@@ -117,7 +117,6 @@ class ML:
     def get_sklearn_model(self, ml_model_params, train_data):
         ''' from model name string specified in conf file, here we get the actual sklearn model obj '''
         return autosklearn.classification.AutoSklearnClassifier(
-            time_left_for_this_task=62000,
             metric=[autosklearn.metrics.roc_auc],
             include = {
                 'classifier': ["random_forest", "gradient_boosting", "decision_tree", "extra_trees", "k_nearest_neighbors", "random_forest", "adaboost"],
